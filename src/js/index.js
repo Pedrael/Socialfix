d7a82cc9da464c8f95fd84198a93fbae5f561102
Pedrael <mihail_krivic@mail.ru>
2020-05-14 20:53:34 +0300

    added phones and we did

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 import jQuery from 'jquery';
 import Header from './header.js';
 import Carousel from './carousel.js';
+import TeaserCarousel from './carousel-teaser.js';
 import Mail from './mail.js';
 
 Header();
 Carousel();
+TeaserCarousel();
 Mail();
